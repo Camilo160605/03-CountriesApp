364ce51243d40d58358ee47620ba236d18d7db05
Camilo160605 <camilo1665ober@gmail.com>
2023-10-25 18:24:09 -0500

    error has been catched

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
+import { CountriesServices } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-by-capital-page',
   templateUrl: './by-capital-page.component.html',
   styles: [
+
   ]
 })
 export class ByCapitalPageComponent {
 
+  public countries : Country[] = []
+
+  constructor(private countriesServices : CountriesServices){}
+
 
   searchByCapital( term : string): void{
-    console.log('Desde capital Page component');
-    console.log(term);
+    this.countriesServices.searchCapital(term)
+    .subscribe(countries =>{
+      this.countries = countries
+      console.log(countries);
+    })
   }
 }
---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Country } from '../interfaces/country';
+import { Observable, catchError, of } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+
+
+export class CountriesServices {
+
+    private apiUrl : string = "https://restcountries.com/v3.1"
+
+
+    constructor(private http: HttpClient) { }
+
+
+
+    searchCapital ( term : string ) : Observable<Country[]>{
+        return this.http.get<Country[]>(`${this.apiUrl}/capital/${term}`)
+        .pipe(
+            catchError( ()=>of ([]) )
+        );
+    }
+}
